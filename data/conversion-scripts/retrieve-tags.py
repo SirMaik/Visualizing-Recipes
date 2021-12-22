9b48a6a44849b29069425f836a4c3e929625d930
SirMaik <mianorsi@gmail.com>
2021-12-22 10:26:40 -0600

    Retrieved tags

---
+++ data/conversion-scripts/retrieve-tags.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import ast
+import csv
+
+inputfile='../kaggle-data/RAW_recipes.csv'
+outputfile='../derived-data/tags.csv'
+
+
+df = pd.read_csv(inputfile , usecols=["tags"])
+counts = {}
+
+for _, series in df.iterrows():
+    tagList = ast.literal_eval(series.tags)
+    for tag in tagList:
+        counts[tag] =  counts.get(tag, 0) + 1
+
+print("There are {} tags.".format(len(counts)))
+
+
+rows = []
+for tag, count in counts.items():
+    rows.append([tag, count])
+
+header = ['tag', 'count']
+sortedRows = rows.sort(reverse=True, key=lambda x:x[1])
+
+with open(outputfile, "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(rows)
