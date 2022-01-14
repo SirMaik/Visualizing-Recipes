ea6bd2fedf21ac0a387aed0e7e5ecb4d7ba69cf5
clarissa2 <clarissa.auckenthaler@gmail.com>
2022-01-14 20:50:53 +0100

    Upload flaskserver, html,css,js; including world_map (from SirMaik), moved csv.files and notbooks to data folder

---
+++ flaskServer.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, request
+import random
+import json
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def index():
+    return redirect(url_for('base'))
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.route('/world_map')
+def world_map():
+    return render_template('world_map.html')
+
+
+if __name__ == '__main__':
+   app.run(debug=True, port=5000)
