ec4135c58b18e67cbd964560d15ea19fa3a79a7c
clarissa2 <clarissa.auckenthaler@gmail.com>
2022-01-17 13:21:50 +0100

    Add some code for recipe search; fixed Word map display with SirMaik

--- static/base.js
+++ static/base.js
@@ -1,12 +1,16 @@
 /*
     @author clarissa2
 */
+
+//Table Content
+//load dataset
 const csvURL= "https://raw.githubusercontent.com/SirMaik/Visualizing-Recipes/main/world_map/data/countries.csv";
 
  d3.csv(csvURL).then(function (data) {
+   //set columnames
     var columns = ['country','code','tag','number_of_recipes']
     console.log(data);
-
+  //create table
     var table = d3.select('div#searchTable').append('table')
     var thead = table.append('thead')
     var tbody = table.append('tbody')
@@ -23,6 +27,7 @@ const csvURL= "https://raw.githubusercontent.com/SirMaik/Visualizing-Recipes/mai
         .enter()
       .append('tr')
 
+    //fill table 
     var cells = rows.selectAll('td')
         .data(function(row) {
             return columns.map(function (column) {
@@ -34,3 +39,27 @@ const csvURL= "https://raw.githubusercontent.com/SirMaik/Visualizing-Recipes/mai
       .text(function (d) { return d.value })
   return table;
   })
+
+  //search functions
+
+  function recipeSearch(){
+    var cuisine = document.getElementById("cuisineInput").value;
+    var ingredient = document.getElementById("ingredientInput").value;
+    var recipeTitle = document.getElementById("recipetitleInput").value;
+
+    document.getElementById("userSearch").style.display = "block"
+
+    //print filter options into html
+    document.getElementById("usercuisine").innerHTML += cuisine;
+    document.getElementById("useringredient").innerHTML += ingredient;
+    document.getElementById("userrecipetitle").innerHTML += recipeTitle;
+  }
+  
+  // Overlay effect table
+  function on() {
+    document.getElementById("tableOverlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("tableOverlay").style.display = "none";
+  }
